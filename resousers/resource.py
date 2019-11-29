ce121dd9b88b571bee2d86de8ad23c9521fe03c0
Dmitriy.Savchenko <Cava0202695677>
2019-11-29 22:03:40 +0200

    added resource

---
+++ resousers/resource.py
@@ -0,0 +1,94 @@
+
+from flask_restful import Resource
+from flask import request, jsonify
+from models.models import *
+from shema.shema import *
+
+
+class UserResurse(Resource):
+
+    def get(self, id=None):
+        if not id:
+            objects = Users.objects
+            return UsersSchema().dump(objects, many=True)
+        return UsersSchema().dump(Users.objects(id=id).get())
+
+    def post(self):
+        error = UsersSchema().validate(request.json)
+        if error:
+            return error
+        obj = Users(**request.json).save()
+        return UsersSchema().dump(Users.objects(id=obj.id).get())
+
+    def put(self, id):
+        obj = Users.objects(id=id).get()
+        obj.update(**request.json)
+        return UsersSchema().dump(obj.reload())
+
+    def delete(self, id):
+        obj = Users.objects(id=id).delete()
+        if obj == 1:
+            return jsonify(**{'delete': 'ok'})
+        else:
+            return jsonify(**{'delete': 'Error'})
+
+
+class ProductResurse(Resource):
+
+    def get(self, id=None):
+        print(request.headers)
+        re = dict(request.headers)
+        print(re['Key'])
+
+        if not id:
+            objects = Product.objects
+            return ProductSchema().dump(objects, many=True)
+        return ProductSchema().dump(Product.objects(id=id).get())
+
+    def post(self):
+        print(request)
+        error = ProductSchema().validate(request.json)
+        if error:
+            return error
+        obj = Product(**request.json).save()
+        return ProductSchema().dump(Product.objects(id=obj.id).get())
+
+    def put(self, id):
+        obj = Product.objects(id=id).get()
+        obj.update(**request.json)
+        return ProductSchema().dump(obj.reload())
+
+    def delete(self, id):
+        obj = Product.objects(id=id).delete()
+        if obj == 1:
+            return jsonify(**{'delete': 'ok'})
+        else:
+            return jsonify(**{'delete': 'Error'})
+
+
+class CategoryResurse(Resource):
+
+    def get(self, id=None):
+        if not id:
+            objects = Category.objects
+            return CategorySchema().dump(objects, many=True)
+        return CategorySchema().dump(Category.objects(id=id).get())
+
+    def post(self):
+        error = CategorySchema().validate(request.json)
+        if error:
+            return error
+        obj = Category(**request.json).save()
+        return CategorySchema().dump(Category.objects(id=obj.id).get())
+
+    def put(self, id):
+        obj = Category.objects(id=id).get()
+        obj.update(**request.json)
+        return CategorySchema().dump(obj.reload())
+
+    def delete(self, id):
+        obj = Category.objects(id=id).delete()
+        if obj == 1:
+            return jsonify(**{'delete': 'ok'})
+        else:
+            return jsonify(**{'delete': 'Error'})
---
+++ shema/shema.py
@@ -0,0 +1,55 @@
+from marshmallow import fields, Schema, validates, ValidationError
+
+
+class PropertiesSchema(Schema):
+    weight = fields.Float(min_value=0)
+
+
+class TextsSchema(Schema):
+    title = fields.String(unique=True)
+    body = fields.String()
+
+
+class LazyCatScheme(Schema):
+    id = fields.String()
+    title = fields.String()
+    description = fields.String()
+    parent = fields.Nested('self')
+
+
+class CategorySchema(Schema):
+    id = fields.String()
+    title = fields.String(required=True)
+    description = fields.String()
+    subcategory = fields.List(fields.Nested(LazyCatScheme), load_only=True)
+    parent = fields.Nested('self', load_only=True)
+
+
+class ProductSchema(Schema):
+    title = fields.String()
+    description = fields.String()
+    price = fields.Integer()
+    new_prise = fields.Integer()
+    is_discount = fields.Bool()
+    properties = fields.Nested(PropertiesSchema)
+    category = fields.Nested(CategorySchema)
+    photo_product = fields.String()
+
+
+class UsersSchema(Schema):
+    first_name = fields.String(required=True)
+    username = fields.String(required=True)
+    last_name = fields.String(required=True)
+    user_id = fields.Integer(required=True)
+    basket = fields.List(fields.Nested(ProductSchema))
+
+
+class BasketSchema(Schema):
+    user = fields.Nested(UsersSchema)
+    basket_list = fields.List(fields.Nested(ProductSchema))
+    bought = fields.Bool(default=False)
+    bought_date = fields.DateTime()
+
+
+class BasketHistorySchema(Schema):
+    basket = fields.Nested(BasketSchema)
