93c5763cd5356ae14df0176ab15965d3690b4f7b
Dmitriy.Savchenko <Cava0202695677>
2019-11-29 21:43:06 +0200

    I  created REST and corrected problems

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import config
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
-from models.models import Category, Product, Texts, Users, Basket, BasketHistory
 import keyboards
 from models import models
+from flask_restful import Api
 from keyboards import ReplyKB
 from flask import Flask, request, abort
+from resousers.resource import CategoryResurse, ProductResurse
+from models.models import Category, Product, Texts, Users, Basket, BasketHistory
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
+
+
 bot = telebot.TeleBot(config.TOKEN)
 app = Flask(__name__)
+api = Api(app)
 
-# Process webhook calls
-@app.route(config.handle_url, methods=['POST'])
+api.add_resource(CategoryResurse, '/Category/', '/Category/<string:id>')
+api.add_resource(ProductResurse, '/Product/', '/Product/<string:id>')
+
+#Process webhook calls
+@app.route('/', methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
@@ -20,15 +28,8 @@ def webhook():
     else:
         abort(403)
 
-
-hideBoard = ReplyKeyboardRemove()  # if sent as reply_markup, will hide the keyboard
-# @bot.message_handler(func=lambda message: message.text)
-# def test(message):
-#     print(message)
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    print(message)
     user_dict = {
         'first_name': message.chat.first_name,
         'username': message.chat.username,
@@ -36,7 +37,6 @@ def start(message):
         'user_id': message.chat.id,
     }
     keyboard = ReplyKB().generate_kb(*keyboards.beginning_kb.values())
-    # print('Users.objects.get(user_id=message.chat.id)', Users.objects.get(user_id=message.chat.id))
     try:
         if Users.objects.get(user_id=message.chat.id):
             user = Users.objects.get(user_id=message.chat.id)
@@ -44,9 +44,15 @@ def start(message):
         else:
             Users(**user_dict).save()
             bot.send_message(message.chat.id, text=f'Hello new buyer {message.chat.username}', reply_markup=keyboard)
+            user = Users.objects.get(user_id=message.chat.id)
+            basket = Basket(user=user)
+            basket.save()
     except Exception:
         Users(**user_dict).save()
         bot.send_message(message.chat.id, text=f'Hello new buyer {message.chat.username}', reply_markup=keyboard)
+        user = Users.objects.get(user_id=message.chat.id)
+        basket = Basket(user=user)
+        basket.save()
 
 
 @bot.message_handler(func=lambda message: message.text == keyboards.beginning_kb['products'])
@@ -113,16 +119,12 @@ def test(call):
     obj_id = call.data.split('_')[1]
     product = Product.objects.get(id=obj_id)
     user = Users.objects.get(user_id=call.message.chat.id)
-    try:
-        basket = Basket.objects.get(user=user)
-        if basket and basket.bought == False:
-            basket.add_product(product)
-            basket.save()
-        else:
-            basket = Basket(user=user)
-            basket.add_product(product)
-            basket.save()
-    except:
+    basket = Basket.objects(user=user, bought=False)
+    if basket:
+        basket = Basket.objects.get(user=user, bought=False)
+        basket.add_product(product)
+        basket.save()
+    else:
         basket = Basket(user=user)
         basket.add_product(product)
         basket.save()
@@ -134,17 +136,14 @@ def show_basket(call):
     if len(basket.basket_list) == 0:
         bot.send_message(call.message.chat.id, 'Карзина пуста Добавте товар.')
     else:
+        bot.send_message(chat_id=call.message.chat.id, text='Карзина')
         for product in basket.basket_list:
-            photo = product.photo_product.read()
             keyboard = InlineKeyboardMarkup(row_width=2)
             buttons = [InlineKeyboardButton(f'{i}', callback_data=f'{key}_{product.id}')
                        for key, i in keyboards.basket_kb.items()]
             keyboard.add(*buttons)
-            bot.send_photo(chat_id=call.message.chat.id, photo=photo,
-                           caption=f'<b>{product.title}</b>  Цена: <i> {product.get_price} 💵 </i> '
-                                   f' <code> {product.description} </code>',
-                           reply_markup=keyboard,
-                           parse_mode='HTML')
+            bot.send_message(chat_id=call.message.chat.id, text=f'{product.title} Цена: {product.get_price} 💵',
+                             reply_markup=keyboard)
     if len(basket.basket_list):
         keyboard = InlineKeyboardMarkup(row_width=2)
         buttons = [InlineKeyboardButton(f'{i}', callback_data=f'{key}_{basket.id}')
@@ -153,16 +152,16 @@ def show_basket(call):
         bot.send_message(chat_id=call.message.chat.id, text=f'Стоимость всех товаров {basket.total_cost()} грн',
                          reply_markup=keyboard,)
 
+
 @bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'Buy')
 def buy_basket(call):
     user = Users.objects.get(user_id=call.message.chat.id)
     basket = Basket.objects.get(id=call.data.split('_')[1])
     basket.basket_closed
-    BasketHistory(basket_list=basket).save()
-    user.update(basket=basket)
+    basket_history = BasketHistory(basket_list=basket).save()
+    user.update(basket=basket_history)
     bot.send_message(chat_id=call.message.chat.id, text=f'Спасибо за покупку')
     print('buy_basket')
-
 
 
 @bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'clear')
@@ -171,7 +170,8 @@ def clear_basket(call):
     basket = Basket.objects.get(id=call.data.split('_')[1])
     if not basket.bought:
         basket.delete()
-        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+        for i in range(len(basket.basket_list)+1):
+            bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id-i)
         print('clear_basket')
     else:
         bot.send_message(chat_id=call.message.chat.id, text=f'Эта карзина уже была купленна {basket.bought_date}.')
@@ -187,75 +187,33 @@ def delete_product(call):
     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
 
 
-
-    # obj_id = call.data.split('_')[1]
-    # print('obj_id', obj_id)
-    # product = Product.objects.get(id=obj_id)
-    # print(product.title)
-    # print(call.message.chat.id)
-    # user = Users.objects.get(user_id=call.message.chat.id)
-    # basket = Basket(user=user)
-    # basket.add_product(product)
-    # basket.save()
-    # print(basket.user.id)
-
-    # basket_kb = {
-    #     'news': 'Последние новости',
-    #     'products': 'Продукты',
-    #     'sales': 'Продукты со скидкой',
-    #     'about': 'информация о магазине',
-    #     'basket': f'в корзине {user.count_product()} товара'
-    # }
-    # keyboard = ReplyKB().generate_kb(*basket_kb.values())
-    # bot.send_message(call.message.chat.id, '👍' ,reply_markup=keyboard)
-    # print(call.data)
+@bot.message_handler(func=lambda message: message.text == keyboards.beginning_kb['news'])
+def inline(message):
+    print('news')
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    text_obj = Texts.objects()
+    buttons = [InlineKeyboardButton(f'{text.title}:{text.body}', callback_data=str(text.title)) for text in text_obj]
+    keyboard.add(*buttons)
+    bot.send_message(message.chat.id, message.text, reply_markup=keyboard)
 
 
-#
-#
-# @bot.message_handler(func=lambda message: message.text == keyboards.beginning_kb['news'])
-# def inline(message):
-#     keyboard = InlineKeyboardMarkup(row_width=1)
-#     text_obj = Texts.objects()
-#     buttons = [InlineKeyboardButton(f'{text.title}:{text.body}',
-#                                     callback_data=str(text.title)) for text in text_obj]
-#     keyboard.add(*buttons)
-#     bot.send_message(message.chat.id, message.text, reply_markup=keyboard)
-#     # ##
-#     # category_obj_db = Product.objects.get(id='5dc5c79087cf8fe5055b3b30')
-#     # photo = category_obj_db.photo_product.read()
-#     # bot.send_photo(message.chat.id, photo=photo)
-#
+@bot.message_handler(func=lambda message: message.text == keyboards.beginning_kb['sales'])
+def inline(message):
+    print('sales')
+    products = Product.objects(is_discount=True)
+    for product in products:
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        buttons = [InlineKeyboardButton(f'{i}',
+                                        callback_data=f'{key}_{product.id}') for key, i in keyboards.product_kb.items()]
+        keyboard.add(*buttons)
+        bot.send_message(message.chat.id, text=f'{product.title} Цена: {product.get_price} 💵',
+                         reply_markup=keyboard)
+
 
 if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
     import time
     bot.remove_webhook()
     time.sleep(1)
-    bot.set_webhook(config.webhook_url)
+    bot.set_webhook(config.webhook_url, certificate=open('webhook_cert.pem', 'r'))
     app.run(debug=True)
-
-    # try:
-    #     basket = Basket.objects.get(user=user, bought=False)
-    #     print(basket.total_cost())
-    #     print('len=', len(basket.basket_list))
-    #     if len(basket.basket_list) == 0:
-    #         bot.send_message(call.message.chat.id, 'Карзина пуста Добавте товар.')
-    #         print('1111')
-    #     else:
-    #         for product in basket.basket_list:
-    #
-    #             photo = product.photo_product.read()
-    #             keyboard = InlineKeyboardMarkup(row_width=2)
-    #             buttons = [InlineKeyboardButton(f'{i}', callback_data=f'{key}_{product.id}')
-    #                        for key, i in keyboards.basket_kb.items()]
-    #             keyboard.add(*buttons)
-    #             # bot.send_photo(chat_id=call.message.chat.id, photo=photo,
-    #             #                caption=f'<b>{product.title}</b>  Цена: <i> {product.get_price} 💵 </i> '
-    #             #                        f' <code> {product.description} </code>',
-    #             #                reply_markup=keyboard,
-    #             #                parse_mode='HTML')
-    #             #bot.send_message(chat_id=call.message.chat.id, text=basket.total_cost())
-    # except:
-    #     print(Exception)
-    #     print('2222')
-    #     bot.send_message(call.message.chat.id, 'Карзина пуста Добавте товар.')
