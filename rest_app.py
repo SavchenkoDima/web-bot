cbba9dc5f543b31845c9cadf51a8ca4255ff3505
Dmitriy.Savchenko <Cava0202695677>
2019-11-30 00:53:34 +0200

    port=config.HOST_PORT)

--- rest_app.py
+++ rest_app.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from flask import Flask
 from resousers.resource import CategoryResurse, ProductResurse
-
+import config
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,4 +10,5 @@ api.add_resource(CategoryResurse, '/Category/', '/Category/<string:id>')
 api.add_resource(ProductResurse, '/Product/', '/Product/<string:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=config.HOST_PORT)
+
