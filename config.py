349d540ae7e4e75e4759fa5a78e0d01e576dba07
Dmitriy.Savchenko <Cava0202695677>
2019-11-29 21:51:52 +0200

    added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '963579635:AAHVPl5995oP_kh2mR1WjOOYFW4nMi9cLck'
+
+#TOKEN = '988106282:AAExz5ZhfNwaIIUW1JMn4JxoLdwHjgIT-fU'
+
+host = '127.0.0.1'
+webhook_url = 'https://217.160.63.182/' #set
+handle_url = ''
+print(TOKEN[::-1])
